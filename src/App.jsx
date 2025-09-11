38edb9cd40f800aaaada2a4b32fd12342099df3b
Aadrash Mehta <mehta.aadrash05@gmail.com>
2025-09-11 23:22:27 +0530

    Updated the media queries

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import GameOver from "./components/GameOver.jsx";
 import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 
 const PLAYERS = {
-    X: 'PLayer 1',
+    X: 'Player 1',
     O: 'Player 2'
   };
 
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -3,7 +3,7 @@ export default function Log({turns}) {
 
  return (
   <ol id="log">
-    {turns.length > 0 ? <h2>Game Log</h2> : null}
+    <h2>Game Log</h2>
     {turns.map((turn) => (
       <li key={`${turn.square.row}${turn.square.col}`}>
         {turn.player} selected {turn.square.row},{turn.square.col}
